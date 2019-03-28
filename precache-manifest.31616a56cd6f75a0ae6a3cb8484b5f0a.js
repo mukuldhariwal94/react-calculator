49b3197b5580ceadd2074efccf9ed4a83327bc98
Dhariwal,Mukul <mukul.dhariwal94@gmail.com>
2019-03-28 12:14:40 +0530

    Updates

---
+++ precache-manifest.31616a56cd6f75a0ae6a3cb8484b5f0a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ad904fae1c82c319c701",
+    "url": "/static/js/main.f62020fc.chunk.js"
+  },
+  {
+    "revision": "3fad22445ec0826e2542",
+    "url": "/static/js/2.aad2bd4a.chunk.js"
+  },
+  {
+    "revision": "ad904fae1c82c319c701",
+    "url": "/static/css/main.e42d80c2.chunk.css"
+  },
+  {
+    "revision": "3fad22445ec0826e2542",
+    "url": "/static/css/2.63a7a527.chunk.css"
+  },
+  {
+    "revision": "bb9c38c8d5d46952f357a29be5e9ef73",
+    "url": "/index.html"
+  }
+];
